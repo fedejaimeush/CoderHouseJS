3e80d33e77e1f725c04773290cfef10338158487
Federico Jaime <fedeajaime@gmail.com>
2021-11-14 16:30:10 -0300

    primer entrega del proyecto final agregado

--- ProyectoFinal/script.js
+++ ProyectoFinal/script.js
@@ -48,6 +48,8 @@ estudiantes.push(est3);
 const est4 = new Estudiante(33597364, "Florencia", "Ciampoli", 34, "1ero 2da", 6.25);
 estudiantes.push(est4);
 
+document.write(`<br/> Las y los estudiantes cargada/os en el sistema son: <br/>`)
+
 for(var estudiante of estudiantes){
     document.write(`<br/>El DNI ${estudiante.dni} corresponde a ${estudiante.nombre} ${estudiante.apellido} de ${estudiante.edad}. Está inscripto al curso ${estudiante.curso} y su promedio es ${estudiante.promedio}.<br/>`)
 }
@@ -81,3 +83,105 @@ estudiantes.sort((estudiante1, estudiante2)=> {
 });
 
 console.log(estudiantes);
+
+//objeto materias
+
+class Materia{
+    constructor(codigo, nombre, curso, docente){
+        this.codigo = codigo; 
+        this.nombre = nombre;
+        this.curso = curso;
+        this.docente = docente; 
+    }
+}
+
+// array materias
+
+const  materias = [];
+
+//funcion que carga nuevas materias
+function cargarMateria(){
+    do{
+        var cargarMateriaInicio = prompt("¿Desea cargar una nueva materia?").toLowerCase();
+        if(cargarMateriaInicio === "no"){
+            break;
+        }else{
+            const materiaCodigo = prompt("Ingresa el código de la materia. Recordá que se construye con las iniciales y el curso. Por ejemplo: BIO12");
+            const materiaNombre = prompt("Ingresa el nombre completo de la materia");
+            const materiaCurso = prompt("Ingresa el curso asociado a la materia");
+            const materiaDocente = prompt("Ingresa el nombre completo del docente a cargo de la materia")
+            materias.push(new Materia(materiaCodigo, materiaNombre, materiaCurso, materiaDocente));
+        }
+    } while (cargarMateriaInicio !== "no")
+}
+
+console.log(materias);
+
+const mat1 = new Materia("BIO11", "Biología", "1ero 1era", "John Doe");
+materias.push(mat1);
+const mat2 = new Materia("CDC11", "Construcción de la Ciudadania", "1ero 1era", "Jane Doe");
+materias.push(mat2);
+const mat3 = new Materia("ING12", "Inglés", "1ero 2da", "Peter Doe");
+materias.push(mat3);
+const mat4 = new Materia("MAT12", "Matemáticas", "1ero 2da", "Maddie Doe");
+materias.push(mat4);
+
+document.write(`<br/> Las materias cargadas en el sistema son: <br/>`)
+
+for(var materia of materias){
+    document.write(`<ul><li>Código: ${materia.codigo}</li>`);
+    document.write(`<li>Nombre: ${materia.nombre}</li>`);
+    document.write(`<li>Curso asignado: ${materia.curso}</li>`);
+    document.write(`<li>Docente a cargo: ${materia.docente}</li></ul>`);
+}
+
+//objeto evaluaciones
+class Evaluacion{
+    constructor(numeroDeOrden, tipo, nota){
+        this.numeroDeOrden = parseFloat(numeroDeOrden);
+        this.tipo = tipo;
+        this.nota = parseFloat(nota);
+    }
+    ponderar(){
+        if(this.tipo === "tp"){
+            return this.nota * 0.6;
+        }else if(this.tipo === "ev"){
+            return this.nota *0.4;
+        }
+    }
+}
+
+//array evaluaciones
+
+const evaluaciones = [];
+
+//funcion que carga nuevas evaluaciones
+function cargarEvaluacion(){
+    do{
+        var cargarEvaluacionInicio = prompt("¿Desea cargar una nueva evaluacion al registro?").toLowerCase();
+        if(cargarEvaluacionInicio === "no"){
+            break;
+        }else{
+            const evaluacionOrden = prompt("Ingresa el N° de orden. Recordá que son 3 dígitos. Por ej: 001");
+            const evaluacionTipo = prompt("Ingresa el tipo de evaluacion. Recuerda indicar TP si es un Trabajo Práctico o EX si es un Examen.").toLowerCase();
+            const evaluacionNota = parseFloat(prompt("Ingresa la nota de la evaluacion"))
+            evaluaciones.push(new Evaluacion(evaluacionOrden, evaluacionTipo, evaluacionNota));
+            }
+    } while (cargarEvaluacionInicio !== "no")
+}
+
+console.log(evaluaciones);
+
+const ev1 = new Evaluacion("001", "tp", 8);
+evaluaciones.push(ev1);
+const ev2 = new Evaluacion("002", "tp", 7);
+evaluaciones.push(ev2);
+const ev3 = new Evaluacion("003", "ev", 9);
+evaluaciones.push(ev3);
+const ev4 = new Evaluacion("004", "ev", 10);
+evaluaciones.push(ev4);
+
+for(var evaluacion of evaluaciones){
+    console.log(`Las notas cargadas en el sistema son: `);
+    console.log(`N° de orden: ${evaluacion.numeroDeOrden}. La nota es ${evaluacion.nota} y su valor ponderado es ${evaluacion.ponderar()}`);
+}
